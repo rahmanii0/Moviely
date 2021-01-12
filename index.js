6cf01fdc183ba470324f445e63e52e7859c0a07a
rahmanii <el_rahmanii@outlook.com>
2021-01-12 23:28:32 +0200

    add rental module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customer');
 const movies = require('./routes/movie');
+const rentals = require('./routes/rental');
 const express = require('express');
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies',movies);
+app.use('/api/rentals', rentals);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
